cdc7717d31ee2a515f08b3bf288514a49a4be84f
keepitkeenan <kea2134@columbia.edu>
2014-11-01 21:31:49 -0400

    Arduino code cycles light corrrectly. Bug fixes for bae.py.

--- bae.py
+++ bae.py
@@ -1,3 +1,8 @@
+# Keenan Albee, George Yu
+# Yhack 11/1/2014
+# Python code for No Sleep November
+# Note: Port in line 21 may require modification
+
 import serial
 import time
 import msvcrt
@@ -9,9 +14,11 @@ YO_API_TOKEN = '5da21321-c977-4643-ae5a-321bda4e3214'
 prevSendPost = -1
 ENDUSER = 'NOTYHACK'
 WAITTIME = 5 # number of seconds of time off pillow before another yo can be sent
+DELAY = 200
+BAUD = 9600
 #open serial port to communicate with galileo
 print "opening serial port lol"
-galileo = serial.Serial(4, 9600, timeout = 1)
+galileo = serial.Serial(5, BAUD, timeout = 1)
 time.sleep(.5) # brief pause to ensure port opened
 
 #galileo sends back a serial stream of digital contact sensor information
@@ -19,17 +26,34 @@ time.sleep(.5) # brief pause to ensure port opened
 #	otherwise: do nothing
 t0 = time.clock() - 11
 prevIn = 0
+counter = 0
+galileo.write('G') # light is on (indicates armed for sending)
 
 while True:
 	nextIn = int(float(galileo.readline()))
+	
+	if int(time.clock() - t0) > WAITTIME:
+		galileo.write('G')
+
 	if (nextIn == 1): # touch has been sensed
 		if int(time.clock() - t0) > WAITTIME: # send a yo if WAITTIME seconds have passed since the serial read last sensed touching
-			requests.post("http://api.justyo.co/yo/", data={'api_token': YO_API_TOKEN , 'username': ENDUSER, 'link': 'http://time.is'})		
+			requests.post("http://api.justyo.co/yo/", data={'api_token': YO_API_TOKEN , 'username': ENDUSER, 'link': "http://6257f35e.ngrok.com"})
+			print "bae alert sent"	
+			galileo.write('K') # light is off (indicates disarmed for sending)
 		t0 = time.clock()
-
-	if requests.get("http://72968c4.ngrok.com/sendnum") == 1:
-		#alarm goes off here
-
+	if counter == DELAY:	
+		counter = 0
+		r = requests.get("http://6257f35e.ngrok.com/sendnum")
+	# if requests.head("http://72968c4.ngrok.com/sendnum", allow_redirects=False):
+		if int(r.text) == 0:
+			print "bae alarm armed"
+		elif int(r.text) == 1:
+			galileo.write('H')
+			print"on"
+			time.sleep(2)
+			galileo.write('L')
+			print"off"
+	counter += 1	
 # jorge's crap below
 # app = Flask(__name__)
 
@@ -63,5 +87,3 @@ while True:
 	# else:
 	# 	sendPost = -1
 	# 	prevSendPost = -1
-
-
--- bae_galileo_v0.ino
+++ bae_galileo_v0.ino
@@ -7,12 +7,14 @@
 // Grove - LED connect to D7
 
 const int pinButton = 3;                        // pin of button define here
-const int pinLed    = 7;                        // pin of led define here
+const int pinLed    = 8;                        // pin of led define here
+const int pinBuzz    = 7;
 int incomingByte;
 
 void setup()
 {
     pinMode(pinButton, INPUT);                  // set button INPUT
+    pinMode(pinBuzz, OUTPUT);
     pinMode(pinLed, OUTPUT);                    // set led OUTPUT
     Serial.begin(9600);
 }
@@ -20,25 +22,27 @@ void setup()
 void loop()
 {
     Serial.println(digitalRead(pinButton));
-    if(digitalRead(pinButton))                     // when button is pressed
-    {
-        digitalWrite(pinLed, HIGH);             // led on
-    }
-    else
-    {
         if (Serial.available() > 0) {
         // read the oldest byte in the serial buffer:
           incomingByte = Serial.read();
         // if it's a capital H (ASCII 72), turn on the LED:
         if (incomingByte == 'H') {
           digitalWrite(pinLed, HIGH);
+          digitalWrite(pinBuzz, HIGH);
         } 
         // if it's an L (ASCII 76) turn off the LED:
         if (incomingByte == 'L') {
           digitalWrite(pinLed, LOW);
+          digitalWrite(pinBuzz, LOW);
+        }
+        
+        if (incomingByte == 'K') {
+          digitalWrite(pinLed, LOW);
+        }
+        if (incomingByte == 'G') {
+          digitalWrite(pinLed, HIGH);
         }
       }  
-    }
     
     delay(10);
 }
